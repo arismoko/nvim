258d47576916bb3b79f531deb03209bef76b8deb
aria figueredo <figueredoaria@gmail.com>
2025-12-30 00:28:14 -0600

    Prune language extras and enable OSC52 clipboard

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -3,3 +3,15 @@
 -- Add any additional options here
 vim.opt.relativenumber = false
 vim.opt.conceallevel = 2
+
+vim.g.clipboard = {
+  name = "OSC 52",
+  copy = {
+    ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+    ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+  },
+  paste = {
+    ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
+    ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
+  },
+}
